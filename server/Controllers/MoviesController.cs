bd221fd2a8bd9f08c6310a883e80f2885446cf84
Danielle444 <kanetidandi@gmail.com>
2025-11-11 18:59:03 +0200

    feat(server): add Movie model and MoviesController

---
+++ server/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            try
+            {
+                return Ok(Movie.Read());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie m)
+        {
+            try
+            {
+                if (m == null) return BadRequest("Movie is null.");
+                bool inserted = m.Insert();
+
+                if (!inserted)
+                    return Conflict("A movie with this Id already exists");
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ server/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿namespace server.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public double Income { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Duration { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public string PhotoUrl { get; set; }
+
+        static public List<Movie> MoviesList = new List<Movie>();
+
+        public bool Insert()
+        {
+            for (int i = 0; i < MoviesList.Count; i++)
+            {
+                if (this.Id == MoviesList[i].Id)
+                {
+                    return false;
+                }
+            }
+            MoviesList.Add(this);
+            return true;
+        }
+        static public List<Movie> Read()
+        {
+            return MoviesList;
+        }
+
+    }
+}
